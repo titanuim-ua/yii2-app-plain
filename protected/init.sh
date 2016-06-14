7e90538cefe2a6adcc22c77e73dbe7e91e222234
Igor Lytvynenko <igor@ert.org.ua>
2016-06-14 12:23:26 +0300

    +init.sh *composer

---
+++ protected/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd $DIR/..
+
+REF=${1:-master}
+
+echo Trying to checkout $REF
+
+git fetch origin
+git reset --hard
+git checkout $REF
+git pull
+
+cd protected;
+
+composer install -n
+#npm install
+
+rm -rf $DIR/../assets/*
+rm -rf $DIR/runtime/cache
+rm -rf $DIR/runtime/debug
+rm -rf $DIR/runtime/gii-*
+rm -rf $DIR/runtime/HTML
+
+echo -en "\n\n\nCurrent commit: "
+git rev-parse HEAD
+
+git log -n 1
