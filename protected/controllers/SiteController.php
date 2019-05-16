c1def59d11c3e0f8e0857c4bdba32cae5ce69e3e
Igor Lytvynenko <igor@ert.org.ua>
2019-05-16 13:10:02 +0300

    php 7.3

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -15,7 +15,7 @@ class SiteController extends Controller
     {
         return [
             'access' => [
-                'class' => AccessControl::className(),
+                'class' => AccessControl::class,
                 'only' => ['logout'],
                 'rules' => [
                     [
@@ -26,7 +26,7 @@ class SiteController extends Controller
                 ],
             ],
             'verbs' => [
-                'class' => VerbFilter::className(),
+                'class' => VerbFilter::class,
                 'actions' => [
                     'logout' => ['post'],
                 ],
